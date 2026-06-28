ff929d56bde5badb63b76337229dbbbc487c7d83
grae ceney <gceney7@gmail.com>
2026-06-28 15:30:52 +0100

    fixed the config so that i can be used on my other machine

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -1,12 +1,19 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
+	vim.fn.mkdir(vim.fn.fnamemodify(lazypath, ":p:h"), "p")
+
+	local clone_result = vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git"
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
 	})
+
+	if vim.v.shell_error ~= 0 then
+		error("Failed to clone lazy.nvim:\n" .. clone_result)
+	end
 end
 
 vim.opt.rtp:prepend(lazypath)
