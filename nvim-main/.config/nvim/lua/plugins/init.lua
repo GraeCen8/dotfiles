1843f741e22675ed38daa497f1db942ce6629e2f
grae ceney <gceney7@gmail.com>
2026-06-28 11:22:42 +0100

    changed the nvchad theme to vim-moonfly-colors

--- nvim-main/.config/nvim/lua/plugins/init.lua
+++ nvim-main/.config/nvim/lua/plugins/init.lua
@@ -3,7 +3,10 @@ return {
   require "plugins.lspconfig",
   require "plugins.lazygit",
   require "plugins.harpoon",
-  require "plugins.flash"
+  require "plugins.flash",
+  require "plugins.persistence",
+  require "plugins.moonfly",
+  require "plugins.telescope",
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 }
