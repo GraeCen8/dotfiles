66b81cfd1f4466f7fd61143272f860f1cb1b9292
grae ceney <gceney7@gmail.com>
2026-06-28 14:02:52 +0100

    formatted options.lua

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,37 +1,51 @@
 local o = vim.opt
 
 -- Line numbers
+
 o.number = true
 o.relativenumber = true
 
 -- Tabs
+
 o.tabstop = 4
 o.shiftwidth = 4
 o.expandtab = true
 o.smartindent = true
 
 -- Searching
+
 o.ignorecase = true
 o.smartcase = true
 
 -- UI
+
 o.signcolumn = "yes"
 o.termguicolors = true
 o.cursorline = true
 
 -- Clipboard
+
 o.clipboard = "unnamedplus"
 
 -- Undo
+
 o.undofile = true
 
 -- Splits
+
 o.splitbelow = true
 o.splitright = true
 
 -- Mouse
+
 o.mouse = "a"
 
 -- Update time
+
 o.updatetime = 200
+
+-- Window
+
+o.winborder = "rounded"
+
 
