656c9f96408b1108db4f972d2b5f481f604de034
grae ceney <gceney7@gmail.com>
2026-07-03 16:52:14 +0100

    added templ lsp and treesitter

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "tailwindcss", "clangd", "ols", "zls", "ts_ls", "svelte", "pyright", "rust_analyzer", "gopls" }
+local servers = { "html", "cssls", "tailwindcss", "clangd", "ols", "zls", "ts_ls", "svelte", "pyright", "rust_analyzer", "gopls", "templ" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
