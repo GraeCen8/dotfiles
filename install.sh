bd70a2d82f0e462c084a4b6fed41507e13277865
grae ceney <gceney7@gmail.com>
2026-07-04 18:04:32 +0100

    added gman a go doc reading tool to

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$ROOT"
 
-stow --target="$HOME" nvim alacritty helix zsh
+stow --target="$HOME" nvim alacritty helix zsh gdoc
