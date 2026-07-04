eebcbedd83f90c60f657debffce3b7585cc960ad
grae ceney <gceney7@gmail.com>
2026-07-04 14:14:16 +0100

    added some more remaps

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -6,5 +6,10 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map("t", "<Esc><Esc>", "<Esc><Esc><C-\\><C-n>", { desc = "exit terminal" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map({'i', 'n', 'v'}, '<C-1>', '\\')
+map({'i', 'n', 'v'}, '<C-2>', '\\n')
+map({'i', 'n', 'v'}, '<C-3>', '|')
+map({'i', 'n', 'v'}, '<C-4>', '~')
