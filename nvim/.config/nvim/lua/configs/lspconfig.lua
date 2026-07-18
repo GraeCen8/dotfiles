cb9f5070d20508da20217033492c2f50ab5cf21b
grae ceney <gceney7@gmail.com>
2026-07-18 10:13:24 +0100

    added stuff to config

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,14 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "clangd", "typescript-language-server", "gopls", "ols" }
+local servers = {
+  "html",
+  "cssls",
+  "clangd",
+  "typescript-language-server",
+  "gopls",
+  "ols",
+  "svelte-language-sever",
+  "tailwind-language-server",
+  "rust-analyzer",
+}
 vim.lsp.enable(servers)
-
--- read :h vim.lsp.config for changing options of lsp servers 
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -29,6 +29,8 @@ return {
         "c",
         "cpp",
         "odin",
+        "svelte",
+        "astro",
       },
     },
   },
@@ -51,6 +53,89 @@ return {
     -- order to load the plugin when the command is run for the first time
     keys = {
       { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      local autopairs = require "nvim-autopairs"
+      autopairs.setup { check_ts = true }
+
+      local html_filetypes = {
+        html = true,
+        typescriptreact = true,
+        javascriptreact = true,
+        svelte = true,
+        astro = true,
+        vue = true,
+        xml = true,
+        heex = true,
+      }
+
+      local group = vim.api.nvim_create_augroup("autopairs-html-tags", { clear = true })
+      vim.api.nvim_create_autocmd("InsertCharPre", {
+        group = group,
+        pattern = "*",
+        callback = function()
+          if vim.v.char ~= ">" then
+            return
+          end
+          if not html_filetypes[vim.bo.filetype] then
+            return
+          end
+          local line = vim.api.nvim_get_current_line()
+          local col = vim.api.nvim_win_get_cursor(0)[2]
+          local before = line:sub(1, col)
+          local tag = before:match("<([%w][%w%-]*)[^>]*$")
+          if not tag then
+            return
+          end
+          vim.schedule(function()
+            local row, c = unpack(vim.api.nvim_win_get_cursor(0))
+            local close_tag = "</" .. tag .. ">"
+            vim.api.nvim_buf_set_text(0, row - 1, c, row - 1, c, { close_tag })
+            vim.api.nvim_win_set_cursor(0, { row, c })
+          end)
+        end,
+      })
+    end,
+  },
+
+  {
+    "mrcjkb/rustaceanvim",
+    -- To avoid being surprised by breaking changes,
+    -- I recommend you set a version range
+    version = "^9",
+    -- This plugin implements proper lazy-loading (see :h lua-plugin-lazy).
+    -- No need to lazy.nvim to lazy-load it.
+    lazy = false,
+  },
+
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    keys = {
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+      { "<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
+      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+    },
+    opts = {},
+  },
+
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
   },
 }
